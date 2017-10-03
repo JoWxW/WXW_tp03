1a7c42c1c7288c0dcde4a928ed34259f556ce404
WANG Xiaowen <wangxian@mac07-m102.utt.fr>
2017-10-03 15:44:10 +0200

    Initial commit

---
+++ app/src/main/java/fr/utt/if26/wxw_tp03/MainActivity.java
@@ -0,0 +1,16 @@
+package fr.utt.if26.wxw_tp03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tx = (TextView)findViewById(R.id.message);
+        tx.setText(R.string.message);
+    }
+}
